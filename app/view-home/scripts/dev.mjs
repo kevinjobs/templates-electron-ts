7cc4b65980f6b71a382250e4c01369bb5831bc94
Kevin Jobs <me@kevinjobs.com>
2023-12-29 09:57:05 +0800

    fix: some bugs

--- app/view-home/scripts/dev.mjs
+++ app/view-home/scripts/dev.mjs
@@ -1,6 +1,7 @@
 #!/usr/bin/env zx
 import { resolve } from 'path';
 
-const config = resolve(__dirname, '../config/webpack.config.js');
+// 在部分系统中路径应当使用 \\ 进行分割
+const config = resolve(__dirname, '../config/webpack.config.js').replaceAll('\\', '\\\\');
 
 await $`cross-env NODE_ENV=development webpack serve -c ${config}`;
